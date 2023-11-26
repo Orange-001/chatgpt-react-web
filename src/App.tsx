18e3109571c5f9f4a44f4690c9544ab1d6c4508d
orange <1347680852@qq.com>
2023-11-26 22:53:08 +0800

    feat: gpt test

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, useRoutes } from 'react-router-dom';
 import routes from './router';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from './store';
-import { Button, Space } from 'antd';
+import { Button, Space, Input, Form, message } from 'antd';
 import { decrement, increment } from './store/features/counter';
+import axios from 'axios';
 
 function App() {
   const { value: counterValue } = useSelector((state: RootState) => state.counter);
   const dispatch = useDispatch<AppDispatch>();
 
+  type FieldType = {
+    message?: string;
+  };
+  const [form] = Form.useForm();
+  const [result, setResult] = useState('');
+
+  const onMessageKeyUp = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.ctrlKey && e.key === 'Enter') {
+      form.submit();
+    }
+  };
+
+  const onFinish = async (values: FieldType) => {
+    if (values.message) {
+      setResult(values.message);
+      // const openaiApiKey = 'your key';
+      // const data = {
+      //   model: 'gpt-3.5-turbo',
+      //   messages: [
+      //     {
+      //       role: 'user',
+      //       content: values.message
+      //     }
+      //   ]
+      // };
+
+      // const headers = {
+      //   'Content-Type': 'application/json',
+      //   Authorization: `Bearer ${openaiApiKey}`
+      // };
+
+      // axios
+      //   .post('https://api.aigc369.com/v1/chat/completions', data, { headers })
+      //   .then((response) => {
+      //     console.log(response.data);
+      //     setResult(response.data.choices[0].message.content);
+      //   })
+      //   .catch((error) => {
+      //     console.error(error);
+      //   });
+    } else {
+      message.warning('Please Input your message!');
+    }
+  };
+
   return (
-    <div>
+    <>
       <h1>App</h1>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div>
@@ -25,7 +71,17 @@ function App() {
         <span>{counterValue}</span>
         <Button onClick={() => dispatch(decrement())}>Deecrement</Button>
       </Space>
-    </div>
+      <Form name="form" form={form} onFinish={onFinish} requiredMark="optional">
+        <Form.Item<FieldType> name="message">
+          <Input.TextArea
+            placeholder="Message ChatGPT..."
+            autoSize={{ minRows: 1, maxRows: 8 }}
+            onKeyUp={onMessageKeyUp}
+          />
+        </Form.Item>
+      </Form>
+      <p>{result}</p>
+    </>
   );
 }
 
