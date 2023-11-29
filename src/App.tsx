2d8f9f274fb0c31a66320671690018a84085d338
Orange-001 <1347680852@qq.com>
2023-11-29 18:42:30 +0800

    feat: upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { NavLink, useRoutes } from 'react-router-dom';
 import routes from './router';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from './store';
-import { Button, Space, Input, Form, message } from 'antd';
+import { Button, Space, Input, Form, message, Upload } from 'antd';
 import { decrement, increment } from './store/features/counter';
 import request from './services';
+import type { UploadProps } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
 
 function App() {
   const { value: counterValue } = useSelector((state: RootState) => state.counter);
@@ -50,21 +52,42 @@ function App() {
     }
   };
 
+  const props: UploadProps = {
+    name: 'file',
+    action: `${process.env.REACT_APP_BASE_URL}/v1/files`,
+    headers: {
+      Authorization: `Bearer ${process.env.REACT_APP_OPENAI_KEY}`
+    },
+    onChange(info) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    }
+  };
+
   return (
     <>
       <h1>App</h1>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
       <div>
         <NavLink to="/">home</NavLink>
         <NavLink to="/settings">settings</NavLink>
       </div>
       <div>{useRoutes(routes)}</div>
+
       <Space size={12}>
         <h2>redux</h2>
         <Button onClick={() => dispatch(increment())}>Increment</Button>
         <span>{counterValue}</span>
         <Button onClick={() => dispatch(decrement())}>Deecrement</Button>
       </Space>
+
       <Form name="form" form={form} onFinish={onFinish} requiredMark="optional">
         <Form.Item<FieldType> name="message">
           <Input.TextArea
@@ -75,6 +98,10 @@ function App() {
         </Form.Item>
       </Form>
       <p>{result}</p>
+
+      <Upload {...props}>
+        <Button icon={<UploadOutlined />}>Click to Upload</Button>
+      </Upload>
     </>
   );
 }
