2e20627d95947cb57cdefe7e5ae7458ccb0f33ab
orange <1347680852@qq.com>
2023-12-04 22:38:25 +0800

    feat: axios SSE请求细节完善

--- src/views/test.tsx
+++ src/views/test.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useState } from 'react';
 import { NavLink, useRoutes } from 'react-router-dom';
 import routes from '@/router';
 import { useDispatch, useSelector } from 'react-redux';
@@ -14,17 +14,23 @@ const Test: FC = () => {
   const { value: counterValue } = useSelector((state: RootState) => state.counter);
   const dispatch = useDispatch<AppDispatch>();
 
+  enum Role {
+    SYSTEM = 'system',
+    USER = 'user',
+    ASSISTANT = 'assistant'
+  }
+  type Message = {
+    role: Role;
+    content: string;
+  };
   type FieldType = {
     message?: string;
-  };
-  type Message = {
-    role: 'system' | 'user' | 'assistant';
-    content: string;
   };
 
   const [form] = Form.useForm();
   const [messages, setMessages] = useState<Message[]>([]);
-  const [result, setResult] = useState('');
+  const [latestReply, setLatestReply] = useState('');
+  const [isFetching, setIsFetching] = useState(false);
 
   const onMessageKeyUp = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (e.ctrlKey && e.key === 'Enter') {
@@ -34,51 +40,49 @@ const Test: FC = () => {
 
   const onFinish = async (values: FieldType) => {
     if (values.message) {
+      const MAX_SEND_MESSAGES_COUNT = 4;
+      const controller = new AbortController();
+      const userMessageTemp: Message = { role: Role.USER, content: values.message };
+      const messagesTemp = [...messages, userMessageTemp].slice(-MAX_SEND_MESSAGES_COUNT);
+      setMessages(messagesTemp);
+      setIsFetching(true);
+
       const data = {
         model: 'gpt-3.5-turbo',
-        messages: [
-          {
-            role: 'user',
-            content: values.message
-          }
-        ],
+        messages: messagesTemp,
         stream: true
       };
 
-      const controller = new AbortController();
+      // 方式一： axios
       request
         .post('v1/chat/completions', data, {
           // warnning: xhr.js:218 The provided value 'stream' is not a valid enum value of type XMLHttpRequestResponseType.
           responseType: 'stream',
           onDownloadProgress: function (progressEvent: AxiosProgressEvent) {
             const responseText: string = progressEvent.event.currentTarget.responseText;
-            console.log(responseText);
 
             // 通过正则表达式提取每个部分的 "content"
             const regex = /"content":"([^"]*)"/g;
             const matches = [...(responseText.matchAll(regex) as any)];
 
             // 将所有匹配的 "content" 拼接成一个新的字符串
-            const resultString = matches.map((match) => match[1]).join('');
+            const latestReplyTemp = matches.map((match) => match[1]).join('');
 
-            setResult(resultString);
+            setLatestReply(latestReplyTemp);
+
+            // 数据接收完毕
+            if (responseText.endsWith('data: [DONE]\n\n')) {
+              const assistantMessageTemp: Message = { role: Role.ASSISTANT, content: latestReplyTemp };
+              const messagesTemp = [...messages, assistantMessageTemp].slice(-MAX_SEND_MESSAGES_COUNT);
+              setMessages(messagesTemp);
+              setIsFetching(false);
+            }
           },
           signal: controller.signal
         })
-        .then((response) => {
-          // console.log(response.data);
-
-          const userMessage: Message = { role: 'user', content: values.message as string };
-          const assistantMessage: Message = {
-            role: 'assistant',
-            // error: 没有获取到最新的result
-            content: result
-          };
-          const newMessages = [...messages, userMessage, assistantMessage].slice(-4);
-          setMessages(newMessages);
-        })
         .catch((error) => {
           console.error(error);
+          setIsFetching(false);
         });
     } else {
       message.warning('Please Input your message!');
@@ -130,10 +134,12 @@ const Test: FC = () => {
           />
         </Form.Item>
       </Form>
+
       <div>
         {messages.map((v, i) => {
           return <p key={i}>{`${v.role}: ${v.content}`}</p>;
         })}
+        {isFetching && <p>{`assistant: ${latestReply}`}</p>}
       </div>
 
       <Upload {...props}>
