fbb5feef5e4dd77e54bc2dfe0b5218c8e2686cff
Orange-001 <1347680852@qq.com>
2023-12-07 17:11:22 +0800

    feat: @fortaine/fetch-event-source实现sse

--- src/views/test.tsx
+++ src/views/test.tsx
@@ -9,6 +9,7 @@ import request from '@/services';
 import type { UploadProps } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import type { AxiosProgressEvent } from 'axios';
+import { EventStreamContentType, fetchEventSource } from '@fortaine/fetch-event-source';
 
 const Test: FC = () => {
   const { value: counterValue } = useSelector((state: RootState) => state.counter);
@@ -38,7 +39,7 @@ const Test: FC = () => {
 
   const onFinish = async (values: FieldType) => {
     if (values.message) {
-      const MAX_SEND_MESSAGES_COUNT = 6;
+      const MAX_SEND_MESSAGES_COUNT = 4;
       const controller = new AbortController();
       const userMessageTemp: Message = { role: Role.USER, content: values.message };
       const messagesTemp = [...messages, userMessageTemp].slice(-MAX_SEND_MESSAGES_COUNT);
@@ -52,7 +53,7 @@ const Test: FC = () => {
       };
 
       try {
-        // 方式一： axios
+        // #region 方式一：axios
         // await request.post('v1/chat/completions', data, {
         //   // warnning: xhr.js:218 The provided value 'stream' is not a valid enum value of type XMLHttpRequestResponseType.
         //   responseType: 'stream',
@@ -77,51 +78,98 @@ const Test: FC = () => {
         //   },
         //   signal: controller.signal
         // });
+        // #endregion
 
-        // 方式二： fetch
+        // #region 方式二：fetch
+        // const { REACT_APP_BASE_URL, REACT_APP_OPENAI_KEY } = process.env;
+        // const res = await fetch(`${REACT_APP_BASE_URL}/v1/chat/completions`, {
+        //   method: 'POST',
+        //   headers: {
+        //     'Content-Type': 'application/json',
+        //     Authorization: `Bearer ${REACT_APP_OPENAI_KEY}`
+        //   },
+        //   body: JSON.stringify(data),
+        //   signal: controller.signal
+        // });
+        // if (res.status === 200 && res.body) {
+        //   const body = res.body;
+        //   const reader = body.getReader();
+        //   let latestReplyTemp = '';
+        //   // eslint-disable-next-line no-constant-condition
+        //   while (true) {
+        //     // 读取分块数据，返回一个 Promise
+        //     // （如果分块可用，Promise 返回 { value: theChunk, done: false } 形式）
+        //     // （如果流已关闭，Promise 返回 { value: undefined, done: true } 形式）
+        //     const { value, done } = await reader.read();
+        //     if (done) {
+        //       break;
+        //     } else {
+        //       // 将分块数据转换为 string
+        //       const responseTextPart = new TextDecoder().decode(value);
+
+        //       // 通过正则表达式提取每个部分的 "content"
+        //       const regex = /"content":"([^"]*)"/g;
+        //       const matches = [...responseTextPart.matchAll(regex)];
+
+        //       // 将所有匹配的 "content" 拼接成一个新的字符串
+        //       const latestReplyPartTemp = matches.map((match) => match[1]).join('');
+        //       latestReplyTemp += latestReplyPartTemp;
+
+        //       const assistantMessageTemp: Message = { role: Role.ASSISTANT, content: latestReplyTemp };
+        //       messagesTemp[latestReplyIndex] = { ...assistantMessageTemp };
+        //       const messagesTemp2 = messagesTemp.slice(-MAX_SEND_MESSAGES_COUNT);
+        //       setMessages(messagesTemp2);
+        //     }
+        //   }
+        // } else {
+        //   Promise.reject(res);
+        // }
+        // #endregion
+
+        // #region 方式三：@fortaine/fetch-event-source
         const { REACT_APP_BASE_URL, REACT_APP_OPENAI_KEY } = process.env;
-        const res = await fetch(`${REACT_APP_BASE_URL}/v1/chat/completions`, {
+        const fetchUrl = `${REACT_APP_BASE_URL}/v1/chat/completions`;
+        let remainText = '';
+
+        fetchEventSource(fetchUrl, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
+            'x-requested-with': 'XMLHttpRequest',
             Authorization: `Bearer ${REACT_APP_OPENAI_KEY}`
           },
           body: JSON.stringify(data),
-          signal: controller.signal
-        });
-        if (res.status === 200 && res.body) {
-          const body = res.body;
-          const reader = body.getReader();
-          let latestReplyTemp = '';
-          // eslint-disable-next-line no-constant-condition
-          while (true) {
-            // 读取分块数据，返回一个 Promise
-            // （如果分块可用，Promise 返回 { value: theChunk, done: false } 形式）
-            // （如果流已关闭，Promise 返回 { value: undefined, done: true } 形式）
-            const { value, done } = await reader.read();
-            if (done) {
-              break;
-            } else {
-              // 将分块数据转换为 string
-              const responseTextPart = new TextDecoder().decode(value);
-
-              // 通过正则表达式提取每个部分的 "content"
-              const regex = /"content":"([^"]*)"/g;
-              const matches = [...responseTextPart.matchAll(regex)];
-
-              // 将所有匹配的 "content" 拼接成一个新的字符串
-              const latestReplyPartTemp = matches.map((match) => match[1]).join('');
-              latestReplyTemp += latestReplyPartTemp;
-
-              const assistantMessageTemp: Message = { role: Role.ASSISTANT, content: latestReplyTemp };
-              messagesTemp[latestReplyIndex] = { ...assistantMessageTemp };
-              const messagesTemp2 = messagesTemp.slice(-MAX_SEND_MESSAGES_COUNT);
-              setMessages(messagesTemp2);
+          signal: controller.signal,
+          // async onopen(res) {},
+          onmessage(msg) {
+            if (msg.data === '[DONE]') {
+              return;
+            }
+            const text = msg.data;
+            try {
+              const json = JSON.parse(text) as {
+                choices: Array<{
+                  delta: {
+                    content: string;
+                  };
+                }>;
+              };
+              const delta = json.choices[0]?.delta?.content;
+              if (delta) {
+                remainText += delta;
+                const assistantMessageTemp: Message = { role: Role.ASSISTANT, content: remainText };
+                messagesTemp[latestReplyIndex] = { ...assistantMessageTemp };
+                const messagesTemp2 = messagesTemp.slice(-MAX_SEND_MESSAGES_COUNT);
+                setMessages(messagesTemp2);
+              }
+            } catch (error) {
+              console.log('[Request] parse error', text);
             }
           }
-        } else {
-          Promise.reject(res);
-        }
+          // onclose() {},
+          // onerror(err) {}
+        });
+        // #endregion
       } catch (error) {
         console.error(error);
       }
